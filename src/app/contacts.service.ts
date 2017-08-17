TheDanMiller/angular2-master-class-jump-start-solutions

step 11: make API_ENDPOINT an InjectionToken

src/app/contacts.service.ts
@@ -1,12 +1,14 @@
 import { Injectable, Inject } from '@angular/core';
 import { Http } from '@angular/http';
 
+import { API_ENDPOINT } from './app.tokens';
+
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ContactsService {
 
-  constructor(private http: Http, @Inject('API_ENDPOINT') private apiEndpoint) {}
+  constructor(private http: Http, @Inject(API_ENDPOINT) private apiEndpoint) {}
 
   getContact (id: string) {
     return this.http.get(`${this.apiEndpoint}/contacts/${id}`)

dmgudeman/thought-ram-master-class-4

src/app/contacts.service.ts
@@ -1,12 +1,14 @@
 import { Injectable, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { API_ENDPOINT } from './app.tokens';
+
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ContactsService {
 
-  constructor(private http: HttpClient, @Inject('API_ENDPOINT') private apiEndpoint) {}
+  constructor(private http: HttpClient, @Inject(API_ENDPOINT) private apiEndpoint) {}
 
   getContact (id: string) {
     return this.http.get<any>(`${this.apiEndpoint}/contacts/${id}`)